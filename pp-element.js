ddcf7ae81e4eb1e7237d5b0a882a35267df9fdd9
carlos-sweb <c4rl0sill3sc4@gmail.com>
2021-08-07 23:42:05 -0400

    Update

---
+++ pp-element.js
@@ -0,0 +1,7 @@
+var addClass = function( styleClass ){
+      this.element.classList.add(styleClass);
+}
+
+var removeClass = function( styleClass ){
+      this.element.classList.remove(styleClass);
+}
